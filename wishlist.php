dc823e51d939b2013664f7cb849c00abef1b8469
keysadika <135772609+keysadika@users.noreply.github.com>
2023-06-22 18:09:25 +0700

    wishlist

---
+++ wishlist.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Khae Kebaya</title>
+
+    <!-- :::::::::::::: icon::::::::::::::-->
+    <link rel="shortcut icon" href="assets/images/logo/khae.png" type="image/png">
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <link rel="stylesheet" href="assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- ...:::: Start Breadcrumb Section:::... -->
+    <div class="breadcrumb-section breadcrumb-bg-color--golden">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="breadcrumb-title">Wishlist</h3>
+                        <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
+                            <nav aria-label="breadcrumb">
+                                <ul>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li>Shop</li>
+                                    <li class="active" aria-current="page">Wishlist</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...:::: End Breadcrumb Section:::... -->
+
+    <!-- ...:::: Start Wishlist Section:::... -->
+    <div class="wishlist-section">
+        <!-- Start Cart Table -->
+        <div class="wishlish-table-wrapper" data-aos="fade-up" data-aos-delay="0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="table_desc">
+                            <div class="table_page table-responsive">
+                                <table>
+                                    <!-- Start Wishlist Table Head -->
+                                    <thead>
+                                        <tr>
+                                            <th class="product_remove">Delete</th>
+                                            <th class="product_thumb">Image</th>
+                                            <th class="product_name">Product</th>
+                                            <th class="product-price">Price</th>
+                                            <th class="product_stock">Stock Status</th>
+                                            <th class="product_addcart">Add To Cart</th>
+                                        </tr>
+                                    </thead> <!-- End Cart Table Head -->
+                                    <tbody>
+                                        <!-- Start Wishlist Single Item-->
+                                        <tr>
+                                            <td class="product_remove"><a href="#"><i class="fa fa-trash-o"></i></a>
+                                            </td>
+                                            <td class="product_thumb"><img src="assets/images/product/1-Kain Lilit Navy Color.jpg" alt=""></a></td>
+                                            <td class="product_name">Kebaya Encim Rose</a></td>
+                                            <td class="product-price">Rp.750.000</td>
+                                            <td class="product_stock">In Stock</td>
+                                            <td class="product_addcart"><a href="#" class="btn btn-md btn-golden"
+                                                    data-bs-toggle="modal" data-bs-target="#modalAddcart">Add To
+                                                    Cart</a></td>
+                                        </tr> <!-- End Wishlist Single Item-->
+                                        <!-- Start Wishlist Single Item-->
+                                        <tr>
+                                            <td class="product_remove"><a href="#"><i class="fa fa-trash-o"></i></a>
+                                            </td>
+                                            <td class="product_thumb"><img src="assets/images/product/2-Classic Kebaya All White Lace.jpg" alt=""></a></td>
+                                            <td class="product_name">Classic Kebaya All White Lace</a></td>
+                                            <td class="product-price">Rp.1.500.000</td>
+                                            <td class="product_stock">In Stock</td>
+                                            <td class="product_addcart"><a href="#" class="btn btn-md btn-golden"
+                                                    data-bs-toggle="modal" data-bs-target="#modalAddcart">Add To
+                                                    Cart</a></td>
+                                        </tr> <!-- End Wishlist Single Item-->
+                                        <!-- Start Wishlist Single Item-->
+                                        <tr>
+                                            <td class="product_remove"><a href="#"><i class="fa fa-trash-o"></i></a>
+                                            </td>
+                                            <td class="product_thumb"><img src="assets/images/product/3-Kebaya Encim Nyonya Kebaya Soft Pink Color.jpg" alt=""></a></td>
+                                            <td class="product_name">Kebaya Encim Soft Pink</a></td>
+                                            <td class="product-price">Rp.700.000</td>
+                                            <td class="product_stock">In Stock</td>
+                                            <td class="product_addcart"><a href="#" class="btn btn-md btn-golden"
+                                                    data-bs-toggle="modal" data-bs-target="#modalAddcart">Add To
+                                                    Cart</a></td>
+                                        </tr> <!-- End Wishlist Single Item-->
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End Cart Table -->
+    </div> <!-- ...:::: End Wishlist Section:::... -->
+
+    <?php include 'footer.php'; ?>
+
+    <!-- material-scrolltop button -->
+    <button class="material-scrolltop" type="button"></button>
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <script src="assets/js/vendor/vendor.min.js"></script>
+    <script src="assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
