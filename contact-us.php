cc157dda762e5dced6598533d18e0e343890515c
Kharisma <93039254+kharsd@users.noreply.github.com>
2023-06-22 18:13:28 +0700

    tambah contact us

---
+++ contact-us.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Khae Kebaya</title>
+
+    <!-- :::::::::::::: icon::::::::::::::-->
+    <link rel="shortcut icon" href="assets/images/logo/khae.png" type="image/png">
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <link rel="stylesheet" href="assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- ...:::: Start Breadcrumb Section:::... -->
+    <div class="breadcrumb-section breadcrumb-bg-color--golden">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="breadcrumb-title">Contact Us</h3>
+                        <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
+                            <nav aria-label="breadcrumb">
+                                <ul>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li class="active" aria-current="page">Contact Us</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...:::: End Breadcrumb Section:::... -->
+
+    <!-- ...::::Start Map Section:::... -->
+    <div class="map-section" data-aos="fade-up" data-aos-delay="0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="mapouter">
+                    <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=surakarta, indonesia&t=&z=14&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2yu.co"></a><br><style>.mapouter{position:relative;text-align:right;height:100%;width:100%;}</style><a href="https://embedgooglemap.2yu.co/">Khae Kebaya</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...::::End  Map Section:::... -->
+
+    <!-- ...::::Start Contact Section:::... -->
+    <div class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <!-- Start Contact Details -->
+                    <div class="contact-details-wrapper section-top-gap-100" data-aos="fade-up" data-aos-delay="0">
+                        <div class="contact-details">
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-phone"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <a href="tel:+0123456789">08123456789</a>
+                                    <a href="tel:+0123456789">081325567812</a>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-globe"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <a href="http://www.example.com/">www.khaekebaya.com</a>
+                                    <a href="mailto:demo@example.com">khae.kebaya@gmail.com</a>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-map-marker"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <span>Surakarta, Kota Surakarta, <br>Jawa Tengah, Indonesia.</span>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                        </div>
+                        <!-- Start Contact Social Link -->
+                        <div class="contact-social">
+                            <h4>Follow Us</h4>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div> <!-- End Contact Social Link -->
+                    </div> <!-- End Contact Details -->
+                </div>
+                <div class="col-lg-8">
+                    <div class="contact-form section-top-gap-100" data-aos="fade-up" data-aos-delay="200">
+                        <h3>Get In Touch</h3>
+                        <form id="contact-form" action="https://htmlmail.hasthemes.com/nazmul/mail.php" method="post">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-name">Name</label>
+                                        <input name="name" type="text" id="contact-name">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-email">Email</label>
+                                        <input name="email" type="email" id="contact-email">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-subject">Subject</label>
+                                        <input name="subject" type="text" id="contact-subject">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-message">Your Message</label>
+                                        <textarea name="message" id="contact-message" cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <button class="btn btn-lg btn-black-default-hover" type="submit">SEND</button>
+                                </div>
+                                <p class="form-messege"></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ...::::ENd Contact Section:::... -->
+
+    <?php include 'footer.php'; ?>
+
+    <!-- material-scrolltop button -->
+    <button class="material-scrolltop" type="button"></button>
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <script src="assets/js/vendor/vendor.min.js"></script>
+    <script src="assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+
+</html>
