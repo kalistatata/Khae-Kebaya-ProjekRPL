73dfc5d47b273eb76b407c767937217ca13b013f
kalistatata <117607079+kalistatata@users.noreply.github.com>
2023-06-22 18:07:33 +0700

    product details

---
+++ product-details-default.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Khae Kebaya</title>
+
+    <!-- ::::::::::::::Favicon icon::::::::::::::-->
+    <link rel="shortcut icon" href="assets/images/logo/khae.png" type="image/png">
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <link rel="stylesheet" href="assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- ...:::: Start Breadcrumb Section:::... -->
+    <div class="breadcrumb-section breadcrumb-bg-color--golden">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="breadcrumb-title">Product Details</h3>
+                        <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
+                            <nav aria-label="breadcrumb">
+                                <ul>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li class="active" aria-current="page">Product Details</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...:::: End Breadcrumb Section:::... -->
+
+    <!-- Start Product Details Section -->
+    <div class="product-details-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-lg-6">
+                    <div class="product-details-gallery-area" data-aos="fade-up" data-aos-delay="0">
+                        <!-- Start Large Image -->
+                        <div class="product-large-image product-large-image-horaizontal swiper-container">
+                            <div class="swiper-wrapper">
+                                <div class="product-image-large-image swiper-slide zoom-image-hover img-responsive">
+                                    <img src="assets/images/product-details/soraya-kebaya-4554-1212203-1.jpg" alt="">
+                                </div>
+                                <div class="product-image-large-image swiper-slide zoom-image-hover img-responsive">
+                                    <img src="assets/images/product-details/soraya-kebaya-4554-1212203-2.jpg" alt="">
+                                </div>
+                                <div class="product-image-large-image swiper-slide zoom-image-hover img-responsive">
+                                    <img src="assets/images/product-details/soraya-kebaya-4554-1212203-3.jpg" alt="">
+                                </div>
+                                <div class="product-image-large-image swiper-slide zoom-image-hover img-responsive">
+                                    <img src="assets/images/product-details/soraya-kebaya-4554-1212203-4.jpg" alt="">
+                                </div>
+                                <div class="product-image-large-image swiper-slide zoom-image-hover img-responsive">
+                                    <img src="assets/images/product-details/soraya-kebaya-4554-1212203-5.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Large Image -->
+                        <!-- Start Thumbnail Image -->
+                        <div
+                            class="product-image-thumb product-image-thumb-horizontal swiper-container pos-relative mt-5">
+                            <div class="swiper-wrapper">
+                                <div class="product-image-thumb-single swiper-slide">
+                                    <img class="img-fluid" src="assets/images/product-details/soraya-kebaya-4554-1212203-2.jpg" alt="">
+                                </div>
+                                <div class="product-image-thumb-single swiper-slide">
+                                    <img class="img-fluid" src="assets/images/product-details/soraya-kebaya-4554-1212203-2.jpg" alt="">
+                                </div>
+                                <div class="product-image-thumb-single swiper-slide">
+                                    <img class="img-fluid" src="assets/images/product-details/soraya-kebaya-4554-1212203-3.jpg" alt="">
+                                </div>
+                                <div class="product-image-thumb-single swiper-slide">
+                                    <img class="img-fluid" src="assets/images/product-details/soraya-kebaya-4554-1212203-4.jpg" alt="">
+                                </div>
+                                <div class="product-image-thumb-single swiper-slide">
+                                    <img class="img-fluid" src="assets/images/product-details/soraya-kebaya-4554-1212203-5.jpg" alt="">
+                                </div>
+                            </div>
+                            <!-- Add Arrows -->
+                            <div class="gallery-thumb-arrow swiper-button-next"></div>
+                            <div class="gallery-thumb-arrow swiper-button-prev"></div>
+                        </div>
+                        <!-- End Thumbnail Image -->
+                    </div>
+                </div>
+                <div class="col-xl-7 col-lg-6">
+                    <div class="product-details-content-area product-details--golden" data-aos="fade-up"
+                        data-aos-delay="200">
+                        <!-- Start  Product Details Text Area-->
+                        <div class="product-details-text">
+                            <h4 class="title">Kebaya Encim Rose Hand Made Embroidery</h4>
+                            <div class="price">Rp.750.000</div>
+                            <p>The Kebaya Encim Rose Hand Made Embroidery is a timeless piece of fashion in sync with tradition that will define and flatter yourself silhouette. This particular piece comes with the most beautiful and intricate lace embroidery handmade along the front and sleeves, charming rose trim and arrow of pearly buttons for a comfortable fit. This Kebaya is made of rayon and is available in white.</p>
+                            <p>Fabric Composition shown all the fabric we are using :<br>1. Rayon<br>2. Linen<br>3. Viscose<br>4. Cotton</p>
+                            <p>Size : Chest Width x Sleeve Length x Length<br>Dimensions : 96 x 54 x 62 Cm</p>
+                        </div> <!-- End  Product Details Text Area-->
+                        <!-- Start Product Variable Area -->
+                        <div class="product-details-variable">
+                            <h4 class="title">Available Options</h4>
+                            <!-- Product Variable Single Item -->
+                            <div class="variable-single-item">
+                                <div class="product-stock"> <span class="product-stock-in"><i class="ion-checkmark-circled"></i></span> 50 IN STOCK</div>
+                            </div>
+                            <!-- Product Variable Single Item -->
+                            <div class="d-flex align-items-center ">
+                                <div class="variable-single-item ">
+                                    <span>Quantity</span>
+                                    <div class="product-variable-quantity">
+                                        <input min="1" max="100" value="1" type="number">
+                                    </div>
+                                </div>
+
+                                <div class="product-add-to-cart-btn">
+                                    <a href="cart.php" class="btn btn-block btn-lg btn-black-default-hover">+ Add To Cart</a>
+                                </div>
+                            </div>
+                            <!-- Start  Product Details Meta Area-->
+                            <div class="product-details-meta mb-20">
+                                <a href="wishlist.php" class="icon-space-right"><i class="icon-heart"></i>Add to wishlist</a>
+                            </div> <!-- End  Product Details Meta Area-->
+                        </div> <!-- End Product Variable Area -->
+
+                        <!-- Start  Product Details Catagories Area-->
+                        <div class="product-details-catagory mb-2">
+                            <span class="title">CATEGORIES:</span>
+                            <ul>
+                                <li><a href="#">Kebaya</a></li>
+                            </ul>
+                        </div> <!-- End  Product Details Catagories Area-->
+                        <!-- Start  Product Details Social Area-->
+                        <div class="product-details-social">
+                            <span class="title">SHARE THIS PRODUCT:</span>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div> <!-- End  Product Details Social Area-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Product Details Section -->
+
+
+    <?php include 'footer.php'; ?>
+
+    <!-- material-scrolltop button -->
+    <button class="material-scrolltop" type="button"></button>
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <script src="assets/js/vendor/vendor.min.js"></script>
+    <script src="assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+
+</html>
